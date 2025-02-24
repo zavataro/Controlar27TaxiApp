ce00d8b882106f9ec9e4bc259d3aa98d45bc732e
zavataro <zavataro@gmail.com>
2025-02-24 15:58:36 -0300

    Atualização Angular 19

--- src/app/components/simple-page/first-page/first-page.component.ts
+++ src/app/components/simple-page/first-page/first-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-first-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './first-page.component.html',
-  styleUrl: './first-page.component.scss'
+    selector: 'app-first-page',
+    imports: [],
+    templateUrl: './first-page.component.html',
+    styleUrl: './first-page.component.scss'
 })
 export class FirstPageComponent {
 
--- src/app/components/simple-page/second-page/second-page.component.ts
+++ src/app/components/simple-page/second-page/second-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-second-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './second-page.component.html',
-  styleUrl: './second-page.component.scss'
+    selector: 'app-second-page',
+    imports: [],
+    templateUrl: './second-page.component.html',
+    styleUrl: './second-page.component.scss'
 })
 export class SecondPageComponent {
 
